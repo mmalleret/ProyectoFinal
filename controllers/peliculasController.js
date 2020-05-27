7dc17dbce71e22ff98bfb2e895037f4dc0a71400
Mmalleret <milimalleret1@gmail.com>
2020-05-27 14:11:18 -0300

    no pudimos avanzar salvo x detalles

--- controllers/peliculasController.js
+++ controllers/peliculasController.js
@@ -1,6 +1,22 @@
 let db = require("../db/models/index");
 let moduloLogin = require('../modulo-login');
 
+async function validarAgregadoResenia(formulario) {
+    let errores = [];
+​
+    let usuario = await moduloLogin.validar(formulario.email, formulario.password);
+​
+    if (usuario == null) {
+        errores.push("Ey! No es valido el usuario!");
+    }
+​
+    if (formulario.reseniaTexto == "") {
+        errores.push("Ey! No me dejes el texto vacio!");
+    }
+​
+    return errores;
+}
+
 
 let peliculasController = {
 
@@ -43,68 +59,79 @@ let peliculasController = {
             id : req.params.idPelicula
         })
     },
-    addReview: function(req, res){
-    moduloLogin.buscarPorEmail(req.body.email)    
-    .then(resultado => {
-        
-        let resenia  = {
-            id_pelicula: req.params.idPelicula,
-            puntaje: req.body.puntaje,
-            texto_de_resenia: req.body.reseniaTexto,
-            id_usuarios: resultado.idusuarios
-            
-
-        
-        }
-
+    addReview: async function(req, res){
+        ​
+        let errores = await validarAgregadoResenia(req.body);
+        ​
+        ​
+        if (errores.length == 0) {
+        ​
+            let resultado = await moduloLogin.buscarPorEmail(req.body.email) 
+                    
+                    let resenia  = {
+                        id_pelicula: req.params.idPelicula,
+                        puntaje: req.body.puntaje,
+                        texto_de_resenia: req.body.reseniaTexto,
+                        id_usuarios: resultado.idusuarios
+                     }
+        ​
             // Guardarla en base de datos....
-            db.Resenia.create(resenia)
-            .then(() => {
-                res.redirect('/peliculas')
-            })
-
-    })    
-        
+            let resenia = await db.Resenia.create(resenia)
+                 res.redirect('/peliculas')    
+        } else {
+                res.send(errores)
+        }
+                
     },
     myReviews: function(req, res){
-        res.render('misResenias',{
-        respuesta:[]    
-        })
-
+            res.render('misResenias',{
+                respuesta:[]    
+            })
+        ​
     }, 
     validationForm: async function(req, res){
         let resultado = await moduloLogin.validar(req.body.email, req.body.password)    
-            if (resultado != null){ 
-                let id  = {
-                usuario: resultado.idusuarios}
-                
-                let respuesta = await db.Resenia.findAll({
-                    where:[
-                        { id_usuarios: id.usuario }
-                    ]
+            console.log(resultado)
+            res.send(resultado)
+        let id  = {
+            usuario: resultado.idusuarios}
+            let respuesta = await db.Resenia.findAll({
+                        where:[
+                            { id_usuarios: id.usuario }
+                            ]
+            })
+                       
+            res.render("misResenias", {
+                    respuesta: respuesta
                 })
-               
-                     res.render("misResenias", {
-                            respuesta: respuesta
-                        })
-                    }else{
-                        res.send("usuario invalido")
-                    }     
-            
-            
-     
-            
+                        
+                    
+    }, 
+        
+    edit : function(req, res){
+        db.Resenia.findByPk(req.query.id)
+        .then((resenia) => {
+            res.render("editar", {
+                resenia: resenia,
+            })
+        })
     
     }, 
-    edit : function(req, res){
-        res.render('editar',{
-            
-        })
+    editReview : function(req, res) {
+        let resenia = {
+            id: req.body.id_pelicula,
+            puntaje: req.body.puntuacion,
+            texto_de_resenia: req.body.reseniaTexto,
+            updatedAt: req.body.fecha,
+        }
 
-    }, 
-    editReview : function(req, res){
-        res.render('editar',{
-            
+        db.Resenia.update(resenia, {
+            where: {
+                idresenia: req.params.id
+            }
+        })
+        .then(() => {
+            res.redirect("/peliculas/misResenias")
         })
 
     }, 
@@ -115,9 +142,13 @@ let peliculasController = {
 
     }, 
     deleteReview : function(req, res){
-        res.render('delete',{
-            
-        })
+
+     db.Resenia.destroy({
+                where: {
+                    idresenia: req.query.id
+                }
+            })
+        .then (()=>{res.redirect("/peliculas/misResenias")})
 
     }, 
 
--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -242,3 +242,39 @@ fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=9fe1abda2acd785b6fc8
 
 
 })
+async function abrirModalDePeli (idDePeli) {
+  document.querySelector("#detalle").click()
+  id = idDePeli
+  let datos = await fetch("https://api.themoviedb.org/3/movie/" + id + "?api_key=9fe1abda2acd785b6fc8d949de634904&language=en-US&page=1")
+
+  pelicula = await datos.json()
+  console.log("----------")
+  console.log(pelicula)
+  console.log("----------")
+  titulo = pelicula.title
+  resumen = pelicula.overview
+
+  fetch('/peliculas/resenias/' +id)
+         .then(function(res){
+           return res.json()
+         })
+         .then(function(resenias){
+          console.log(resenias)
+          document.querySelector(".reseniasPelis").innerHTML = ""
+          for (var i = 0; i < resenias.length; i++) {
+              let reseniasHTML = "<div>"
+              reseniasHTML += resenias[i].texto_de_resenia
+              reseniasHTML += "</div>"
+              document.querySelector(".reseniasPelis").innerHTML += reseniasHTML
+              }
+        })
+        
+         document.querySelector(".el-titular").innerHTML = titulo
+         document.querySelector(".el-resumen").innerHTML = resumen
+         document.querySelector("#elVerMas").style.display = "none"
+         //esta linea de codigo es para agregar el id de la pelicula a la ruta 
+         document.querySelector(".jsBackPlis").innerHTML = '<a href="/peliculas/agregarResenia/' + id + '" > <button class= "uk-button uk-button-default" > Añadir reseñas </button> </a>'
+         document.querySelector("#vamos").style.display = "block"
+         document.querySelector("#vamos").setAttribute("idPelicula", id)
+}
+
